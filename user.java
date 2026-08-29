c0fae133e30cf060875d778a19670d9942ab7641
sfarish1005-sys <s.farish1005@gmail.com>
2026-08-29 22:27:44 +0530

    Update user.java
    fix user class

--- user.java
+++ user.java
@@ -1,25 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+class User {
+    String name;
+    String email;
+    String password;
+    ArrayList<String> cart = new ArrayList<>();
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter your name: ");
-        String name = sc.nextLine();
-
-        System.out.print("Enter your email: ");
-        String email = sc.nextLine();
-
-        System.out.print("Enter your password: ");
-        String password = sc.nextLine();
-
-        User user = new User(name, email, password);
-
-        System.out.println("\nRegistration Successful!");
-        System.out.println("Name: " + user.name);
-        System.out.println("Email: " + user.email);
-
-        sc.close();
+    User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
     }
 }
